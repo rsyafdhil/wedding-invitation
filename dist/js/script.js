afd2b5d94744b2e2517938d39998996cc35035dd
Rassya <rasyafadhil@gmail.com>
2024-02-05 19:40:53 +0700

    add hamburger

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,7 +1,7 @@
-const hamburger = document.querySelector("#hamburger");
-const navMenu = document.querySelector("#nav-menu");
+const navButton = document.getElementById('navbar-button');
+const navList = document.getElementById('navbar-list');
 
-hamburger.addEventListener("click", function () {
-  hamburger.classList.toggle("hamburger-active");
-  navMenu.classList.toggle("hidden");
-});
+navButton.addEventListener('click', ()=>{
+  navButton.classList.toggle('navButton-active');
+  navList.classList.toggle('hidden');
+})
