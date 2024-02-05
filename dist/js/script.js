ad30be55b7e38e0f836b03168da808b5dae0caa6
Rassya <rasyafadhil@gmail.com>
2024-02-05 21:16:17 +0700

    add mail sender

--- dist/js/script.js
+++ dist/js/script.js
@@ -4,4 +4,36 @@ const navList = document.getElementById('navbar-list');
 navButton.addEventListener('click', ()=>{
   navButton.classList.toggle('navButton-active');
   navList.classList.toggle('hidden');
-})
+});
+
+const myForm = document.getElementById('form');
+
+myForm.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    const url = e.target.action;
+
+    const formData = new FormData(myForm);
+
+    fetch(url,{
+        method: 'POST',
+        body: formData,
+        mode: 'no-cors'
+    }).then((response) => {
+      Swal.fire({
+          title: 'Success!',
+          text: 'Email balasan yang berisi kode QR akan dikirim 1x24 jam pada email yang bersangkutan.',
+          timer: 2000,
+          timerProgressBar: true,
+          icon: 'success',
+          iconColor: '#FFBF00',
+          showConfirmButton: false,
+      }).then(() => {
+          myForm.reset()
+          window.location.reload()
+      })
+  }).catch((error) => {
+    console.error('Error occurred:', error);
+    alert('Error occurred. Check the console for details.');
+  });
+});
